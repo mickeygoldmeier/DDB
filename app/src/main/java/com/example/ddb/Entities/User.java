397274b18af6aa6886b294c30e6b0955108c13dc
נתן מנור <natanmanor@outlook.com>
2019-11-13 14:32:03 +0200

    First activity design

--- app/src/main/java/com/example/ddb/Entities/User.java
+++ app/src/main/java/com/example/ddb/Entities/User.java
@@ -10,7 +10,7 @@ public abstract class User {
 
     public User() {}
 
-    public User(String userID, String password, Date lastModified, com.example.ddb.Entities.Address address) {
+    public User(String userID, String password, Date lastModified, Address address) {
         UserID = userID;
         Password = password;
         LastModified = lastModified;
@@ -23,8 +23,8 @@ public abstract class User {
     public void setPassword(String password) {Password = password; }
     public Date getLastModified() {return LastModified; }
     public void setLastModified(Date lastModified) {LastModified = lastModified;}
-    public com.example.ddb.Entities.Address getAddress() {return Address; }
-    public void setAddress(com.example.ddb.Entities.Address address) {Address = address;}
+    public Address getAddress() {return Address; }
+    public void setAddress(Address address) {Address = address;}
 
     public abstract boolean search(String string);
     public abstract boolean generatePassword();
--- app/src/main/java/com/example/ddb/UI/MainActivity.java
+++ app/src/main/java/com/example/ddb/UI/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.ddb.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.ddb.R;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // hide the Action Bar
+        try {
+            getSupportActionBar().hide();
+        } catch (Exception e) {
+            getActionBar().hide();
+        }
+
+        // set the welcome title
+        TextView welcomeTV = findViewById(R.id.welcome_tv);
+        int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        if (hour > 0 && hour < 5)
+            welcomeTV.setText(R.string.good_night);
+        else if (hour >= 5 && hour < 13)
+            welcomeTV.setText(R.string.good_morning);
+        else if (hour >= 13 && hour < 20)
+            welcomeTV.setText(R.string.good_afternoon);
+        else
+            welcomeTV.setText(R.string.good_evning);
     }
 }
