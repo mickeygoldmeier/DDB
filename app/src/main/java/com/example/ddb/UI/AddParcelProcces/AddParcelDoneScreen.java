33f27f3d1b5838ab625e162b4ac6516879a6d43f
נתן מנור <natanmanor@outlook.com>
2019-11-21 00:01:38 +0200

    Done screen after parcel adding

--- app/src/main/java/com/example/ddb/UI/AddParcelProcces/AddParcelDoneScreen.java
+++ app/src/main/java/com/example/ddb/UI/AddParcelProcces/AddParcelDoneScreen.java
@@ -3,6 +3,8 @@ package com.example.ddb.UI.AddParcelProcces;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -23,5 +25,13 @@ public class AddParcelDoneScreen extends AppCompatActivity {
 
         ImageView gif_iv = findViewById(R.id.gif2_iv);
         Glide.with(this).load(R.drawable.add_pacel_done).into(gif_iv);
+
+        Button close_ok_screen_btn = findViewById(R.id.close_ok_screen_btn);
+        close_ok_screen_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
