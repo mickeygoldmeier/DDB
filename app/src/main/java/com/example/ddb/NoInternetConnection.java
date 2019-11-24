48abf93f972d751b8553fcce54a152ae6a925c5f
נתן מנור <natanmanor@outlook.com>
2019-11-24 21:43:53 +0200

    No Internet screen

---
+++ app/src/main/java/com/example/ddb/NoInternetConnection.java
@@ -0,0 +1,14 @@
+package com.example.ddb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoInternetConnection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_internet_connection);
+    }
+}
