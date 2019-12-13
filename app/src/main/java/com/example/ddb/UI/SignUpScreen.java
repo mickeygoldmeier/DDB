f353926f1ee993f01cebb72824f4e07100ddfece
נתן מנור <natanmanor@outlook.com>
2019-12-13 16:07:01 +0200

    Sign up screen

---
+++ app/src/main/java/com/example/ddb/UI/SignUpScreen.java
@@ -0,0 +1,44 @@
+package com.example.ddb.UI;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ddb.R;
+
+public class SignUpScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_screen);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        getSupportActionBar().setTitle("");
+
+        Button sendEmail = findViewById(R.id.send_mail_btn);
+        sendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("message/rfc822");
+                i.putExtra(Intent.EXTRA_EMAIL, new String[]{"natanmanor@gmail.com"});
+                i.putExtra(Intent.EXTRA_SUBJECT, "subject");
+                i.putExtra(Intent.EXTRA_TEXT, "text");
+                try {
+                    startActivity(Intent.createChooser(i, "send mail"));
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
